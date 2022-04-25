c80caf83f83e0c4cefba7519d0bb32b8ccb4f1fc
NicoleSF <psi.fagundes@gmail.com>
2022-04-25 15:46:15 -0300

    refatorando o código e os testes

--- src/troco.test.ts
+++ src/troco.test.ts
@@ -1,25 +1,105 @@
 import { calcularTroco } from "./troco";
+import { calcularMoedasNotas } from "./troco";
+
+describe("Função de troco", () => {
+  test("A função deve retornar o troco", () => {
+    const resultado = calcularTroco(100, 84.6);
+    expect(resultado).toBe(15.4);
+  });
+});
+
+describe("Função para calcular a quantidade de notas e moedas no troco", () => {
+  test("A função deve retornar as notas de 100, 50, 20, 10 e 5 para o troco de 187", () => {
+    const resultado = calcularMoedasNotas(calcularTroco(310, 123));
+    const resultadoEsperado = [
+      "É necessário 1 nota(s) de 100 reais",
+      "É necessário 1 nota(s) de 50 reais",
+      "É necessário 1 nota(s) de 20 reais",
+      "É necessário 1 nota(s) de 10 reais",
+      "É necessário 1 nota(s) de 5 reais",
+      "É necessário 1 nota(s) de 2 reais",
+    ];
+    expect(resultado).toEqual(resultadoEsperado);
+  });
+
+  test("A função deve retornar moedas de 1, 0.50, 0.25, 0.10, 0.05 para o troco de 1.95", () => {
+    const resultado = calcularMoedasNotas(calcularTroco(2, 0.05));
+    const resultadoEsperado = [
+      "É necessário 1 moeda(s) de 1 real/centavos",
+      "É necessário 1 moeda(s) de 0.5 real/centavos",
+      "É necessário 1 moeda(s) de 0.25 real/centavos",
+      "É necessário 2 moeda(s) de 0.1 real/centavos",
+    ];
+    expect(resultado).toEqual(resultadoEsperado);
+  });
+
+  test("A função deve retornar moedas de 0.1 e 0.5 para o troco de 0.15", () => {
+    const resultado = calcularMoedasNotas(calcularTroco(2, 1.85));
+    const resultadoEsperado = [
+      "É necessário 1 moeda(s) de 0.1 real/centavos",
+      "É necessário 1 moeda(s) de 0.05 real/centavos",
+    ];
+    expect(resultado).toEqual(resultadoEsperado);
+  });
+
+  test("A função deve retornar duas notas de 20 para o troco de 40", () => {
+    const resultado = calcularMoedasNotas(calcularTroco(80, 40));
+    const resultadoEsperado = ["É necessário 2 nota(s) de 20 reais"];
+    expect(resultado).toEqual(resultadoEsperado);
+  });
+
+  test("A função deve retornar uma nota de 50 e outra de 10 para o troco de 60", () => {
+    const resultado = calcularMoedasNotas(calcularTroco(100, 40));
+    const resultadoEsperado = [
+      "É necessário 1 nota(s) de 50 reais",
+      "É necessário 1 nota(s) de 10 reais",
+    ];
+    expect(resultado).toEqual(resultadoEsperado);
+  });
+});
 
 
-describe('A função deve retornar o troco com cédulas e moedas', () => {
 
-    test('A função deve retornar o total do troco', () => {
-        const resultado = calcularTroco(250, 210.20);
-        const resultadoEsperado = ["É necessário 1 nota(s) de 20 reais", "É necessário 1 nota(s) de 10 reais", "É necessário 1 nota(s) de 5 reais", "É necessário 2 nota(s) de 2 reais", "É necessário 1 moeda(s) de 0.5 real/centavos", "É necessário 1 moeda(s) de 0.25 real/centavos", "É necessário 1 moeda(s) de 0.05 real/centavos"]
-        expect(resultado).toEqual(resultadoEsperado);
-    })
 
-    test('A função deve retornar as notas de 100, 50, 20, 10 e 5 para o troco de 185', () => {
-        const resultado = calcularTroco(310, 123);
-        const resultadoEsperado = ["É necessário 1 nota(s) de 100 reais", "É necessário 1 nota(s) de 50 reais","É necessário 1 nota(s) de 20 reais", "É necessário 1 nota(s) de 10 reais", "É necessário 1 nota(s) de 5 reais", "É necessário 1 nota(s) de 2 reais"];
-        expect(resultado).toEqual(resultadoEsperado);
-    })
 
-    test('A função deve retornar moedas de 1, 0.50, 0.25, 0.10, 0.05 para o troco de 1.95', () => {
-        const resultado = calcularTroco(2, 0.05);
-        const resultadoEsperado = ["É necessário 1 moeda(s) de 1 real/centavos","É necessário 1 moeda(s) de 0.5 real/centavos", "É necessário 1 moeda(s) de 0.25 real/centavos", "É necessário 1 moeda(s) de 0.1 real/centavos", "É necessário 2 moeda(s) de 0.05 real/centavos"]
-        expect(resultado).toEqual(resultadoEsperado);
-    })
 
-   
-})
+
+// describe('A função deve retornar o troco com cédulas e moedas', () => {
+
+//     test('A função deve retornar o total do troco', () => {
+//         const resultado = calcularTroco(250, 210.20);
+//         const resultadoEsperado = ["É necessário 1 nota(s) de 20 reais", "É necessário 1 nota(s) de 10 reais", "É necessário 1 nota(s) de 5 reais", "É necessário 2 nota(s) de 2 reais", "É necessário 1 moeda(s) de 0.5 real/centavos", "É necessário 1 moeda(s) de 0.25 real/centavos", "É necessário 1 moeda(s) de 0.05 real/centavos"]
+//         expect(resultado).toEqual(resultadoEsperado);
+//     })
+
+//     test('A função deve retornar as notas de 100, 50, 20, 10 e 5 para o troco de 187', () => {
+//         const resultado = calcularTroco(310, 123);
+//         const resultadoEsperado = ["É necessário 1 nota(s) de 100 reais", "É necessário 1 nota(s) de 50 reais","É necessário 1 nota(s) de 20 reais", "É necessário 1 nota(s) de 10 reais", "É necessário 1 nota(s) de 5 reais", "É necessário 1 nota(s) de 2 reais"];
+//         expect(resultado).toEqual(resultadoEsperado);
+//     })
+
+//     test('A função deve retornar moedas de 1, 0.50, 0.25, 0.10, 0.05 para o troco de 1.95', () => {
+//         const resultado = calcularTroco(2, 0.05);
+//         const resultadoEsperado = ["É necessário 1 moeda(s) de 1 real/centavos","É necessário 1 moeda(s) de 0.5 real/centavos", "É necessário 1 moeda(s) de 0.25 real/centavos", "É necessário 2 moeda(s) de 0.1 real/centavos"];
+//         expect(resultado).toEqual(resultadoEsperado);
+//     })
+
+//     test('A função deve retornar moedas de 0.1 e 0.5 para o troco de 0.15', () => {
+//         const resultado = calcularTroco(2, 1.85);
+//         const resultadoEsperado = ["É necessário 1 moeda(s) de 0.1 real/centavos", "É necessário 1 moeda(s) de 0.05 real/centavos"];
+//         expect(resultado).toEqual(resultadoEsperado);
+//     })
+
+//     test('A função deve retornar duas notas de 20 para o troco de 40', () => {
+//         const resultado = calcularTroco(80, 40);
+//         const resultadoEsperado = ["É necessário 2 nota(s) de 20 reais"];
+//         expect(resultado).toEqual(resultadoEsperado);
+//     })
+
+//     test('A função deve retornar uma nota de 50 e outra de 10 para o troco de 60', () => {
+//         const resultado = calcularTroco(100, 40);
+//         const resultadoEsperado = ["É necessário 1 nota(s) de 50 reais", "É necessário 1 nota(s) de 10 reais"];
+//         expect(resultado).toEqual(resultadoEsperado);
+//     })
+
+// })
--- src/troco.ts
+++ src/troco.ts
@@ -1,40 +1,84 @@
+const moedas: number[] = [1, 0.5, 0.25, 0.1, 0.05];
+const cedulas: number[] = [100, 50, 20, 10, 5, 2];
 
-const moedas:number[] = [1, 0.50, 0.25, 0.10, 0.05];
-const cedulas:number[] = [100, 50, 20, 10, 5, 2];
 
-export function calcularTroco(valorPago: number, valorCompra: number){
 
-    let troco = valorPago - valorCompra;
-    console.log(`Seu troco ficou em R$ ${+troco.toFixed(2)}`);
+export function calcularTroco(valorPago: number, valorCompra: number) {
 
-    let resultado = [];
+  let troco = valorPago - valorCompra;
 
-    for(let i = 0; i < cedulas.length; i++){
-        let quantidadeCedulas = Math.floor(Math.floor(troco) / cedulas[i]); //quantidade de cédulas necessárias para o troco
-        if(quantidadeCedulas > 0){
-             troco = troco % cedulas[i];
-            resultado.push(`É necessário ${quantidadeCedulas} nota(s) de ${cedulas[i]} reais`);
-        }
-        
-    }
-
-    for(let i = 0; i < moedas.length; i++){
-        let quantidadeMoedas = Math.floor(troco / moedas[i]);
-        if(quantidadeMoedas > 0){
-            troco -= quantidadeMoedas * moedas[i];
-            resultado.push(`É necessário ${quantidadeMoedas} moeda(s) de ${moedas[i]} real/centavos`);
-        }
-
-    }
-
-    return resultado;
-
+  return (+troco.toFixed(2));
 }
 
-console.log(calcularTroco(2, 0.05));
+console.log(`Seu troco é ${calcularTroco(100, 78.25)}`);
 
 
 
+export function calcularMoedasNotas(troco: number) {
+
+  let resultado = [];
+
+  for (let i = 0; i < cedulas.length; i++) {
+    let quantidadeCedulas = Math.floor(Math.floor(troco) / cedulas[i]); //quantidade de cédulas necessárias para o troco
+    if (quantidadeCedulas > 0) {
+      troco = troco % cedulas[i];
+      resultado.push(
+        `É necessário ${quantidadeCedulas} nota(s) de ${cedulas[i]} reais`
+      );
+    }
+  }
+
+  for (let i = 0; i < moedas.length; i++) {
+    let quantidadeMoedas = Math.floor(troco / moedas[i]);
+    if (quantidadeMoedas > 0) {
+      troco -= quantidadeMoedas * moedas[i];
+      troco = +troco.toFixed(2);
+      resultado.push(
+        `É necessário ${quantidadeMoedas} moeda(s) de ${moedas[i]} real/centavos`
+      );
+    }
+  }
+
+  return resultado;
+}
+
+console.log(calcularMoedasNotas(calcularTroco(100, 78.25)));
+
+
+
+
+// export function calcularTroco(valorPago: number, valorCompra: number){
+
+//     let troco = valorPago - valorCompra;
+//     console.log(`Seu troco ficou em R$ ${+troco.toFixed(2)}`);
+
+//     let resultado = [];
+
+//     for(let i = 0; i < cedulas.length; i++){
+//         let quantidadeCedulas = Math.floor(Math.floor(troco) / cedulas[i]); //quantidade de cédulas necessárias para o troco
+//         if(quantidadeCedulas > 0){
+//              troco = troco % cedulas[i];
+//             resultado.push(`É necessário ${quantidadeCedulas} nota(s) de ${cedulas[i]} reais`);
+//         }
+
+//     }
+
+//     for(let i = 0; i < moedas.length; i++){
+//         let quantidadeMoedas = Math.floor(troco / moedas[i]);
+//         if(quantidadeMoedas > 0){
+//             troco -= quantidadeMoedas * moedas[i];
+//             troco = +troco.toFixed(2);
+//             console.log(troco);
+//             resultado.push(`É necessário ${quantidadeMoedas} moeda(s) de ${moedas[i]} real/centavos`);
+//         }
+
+//     }
+
+//     return resultado;
+
+// }
+
+// console.log(calcularTroco(100, 40));
 
 // FUNÇÃO SEPARADA DAS CÉDULAS
 
@@ -42,7 +86,7 @@ console.log(calcularTroco(2, 0.05));
 
 //     let troco = valorPago - valorCompra;
 //     let resultado = [];
-   
+
 //     for(let i = 0; i < cedulas.length; i++){
 //         let quantidadeCedulas = Math.floor(Math.floor(troco) / cedulas[i]); //quantidade de cédulas necessárias para o troco
 //         if(quantidadeCedulas > 0){
@@ -50,21 +94,19 @@ console.log(calcularTroco(2, 0.05));
 //             //resultado.push({quantidade: quantidadeCedulas, valor: cedulas[i]}) valor são as notas
 //             resultado.push(`Ficou a quantidade de ${quantidadeCedulas} nota(s) de ${cedulas[i]} reais`);
 //         }
-        
+
 //     }
-    
+
 //     return resultado;
 // }
 
 // console.log(`O valor do troco é:`, contarCedulas(300, 235));
 
-
-
 //FUNÇÃO SEPARADA DAS MOEDAS
 
 // function contarMoedas(valorPago: number, valorCompra: number){
 
-//     let troco = valorPago - valorCompra; 
+//     let troco = valorPago - valorCompra;
 //     let resultado = [];
 
 //     for(let i = 0; i < moedas.length; i++){
@@ -83,4 +125,3 @@ console.log(calcularTroco(2, 0.05));
 // }
 
 // console.log(contarMoedas(8, 3.70));
-
